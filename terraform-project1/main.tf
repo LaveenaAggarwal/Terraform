edc771abfb4b88ffcd985f90ef5e18ad00fea18d
laveena aggarwal <er.laveenaaggarwal08@gmail.com>
2025-05-30 17:09:04 +0530

    changes

--- terraform-project1/main.tf
+++ terraform-project1/main.tf
@@ -97,7 +97,7 @@ resource "null_resource" "repo" {
       "sudo yum install -y git python3 python3-pip",
       "cd /home/ec2-user && if [ -d 'Python' ]; then cd Python && git pull origin develop; else git clone https://github.com/LaveenaAggarwal/Python.git; fi",
       "cd /home/ec2-user/Python/Python-Project1 && sudo pip3 install -r requirements.txt",
-      "cd /home/ec2-user/Python/Python-Project1 && sudo python3 main.py &"
+      "cd /home/ec2-user/Python/Python-Project1 && nohup sudo python3 main.py &"
     ]
   }
 }
